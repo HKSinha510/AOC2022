2fc310edca283ceeb3918d4df7879be02657c7d7
Hardik Kumar Sinha <hardikkumarsinha@gmail.com>
2023-10-17 19:55:42 +0530

    finished day2 part 2

--- day2/main.py
+++ day2/main.py
@@ -29,18 +29,19 @@ def solution() -> None:
 
     key_mapping = {"X": "A","Y": "B","Z": "C"}
     point_mapping = {"AA": 3+1, "BB": 3+2, "CC": 3+3, "AB": 6+2, "AC": 0+3, "BA": 0+1, "BC": 6+3, "CA": 6+1, "CB": 0+2}
+    point_mapping2 = {"AA": 0+3, "BB": 3+2, "CC": 6+1, "AB": 3+1, "AC": 6+2, "BA": 0+1, "BC": 6+3, "CA": 0+2, "CB": 3+3}
 
     point = 0
+    point2=0
     for i in give_data():
         player = i[0]
         oppo = key_mapping[i[1]]
         
         point += point_mapping[player+oppo]
-
-        
-
+        point2 += point_mapping2[player+oppo]
 
     print(point)
+    print(point2)
 
 if __name__ == "__main__":
     solution()
